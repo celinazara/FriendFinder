c8d633e5f13d9c0ca1177419025c4bfbd694c549
Celina <celina.zaragoza@gmail.com>
2018-02-16 12:07:44 -0600

    htmlRoutes

---
+++ routing/htmlRoutes.js
@@ -0,0 +1,16 @@
+//Dependencies
+var path = require('path');
+
+//routing
+
+module.exports = function(app) {
+	app.get('/survey', function( req, res) {
+		res.sendFile(path.join(__dirname + '/../public/survey.html'));
+	});
+
+	//connecting route to homepage
+	app.use(function(req, res) {
+		res.sendFile(path.join(__dirname + '/..public/home.html'));
+
+	});
+};
