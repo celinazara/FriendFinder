59edc7f79a573f53cd890b954cdaa0f343c1b1b7
Celina <celina.zaragoza@gmail.com>
2018-03-12 18:22:19 -0500

    added modal need to add picture to profile

---
+++ app/data/friends.js
@@ -0,0 +1,77 @@
+//Friends array
+//initialize starting with 'dummy' data
+
+var friendList = [
+    {
+        name: 'Celina',
+        profilePic: 'http://i.pravatar.cc/{250}',
+        scores: [
+            "4",
+            "5",
+            "5",
+            "2",
+            "4",
+            "5",
+            "4",
+            "4",
+            "3",
+            "4"
+        ]
+    }
+]
+
+var friendList = [
+    {
+        name: 'Chantal',
+        profilePic: 'http://i.pravatar.cc/{250}',
+        scores: [
+            "4",
+            "5",
+            "5",
+            "2",
+            "4",
+            "5",
+            "4",
+            "4",
+            "3",
+            "4"
+        ]
+    }
+]
+var friendList = [
+    {
+        name: 'Agustin',
+        profilePic: 'http://i.pravatar.cc/{250}',
+        scores: [
+            "4",
+            "5",
+            "5",
+            "2",
+            "4",
+            "5",
+            "4",
+            "4",
+            "3",
+            "4"
+        ]
+    }
+]
+var friendList = [
+    {
+        name: 'Stephen',
+        profilePic: 'http://i.pravatar.cc/{250}',
+        scores: [
+            "4",
+            "5",
+            "5",
+            "2",
+            "4",
+            "5",
+            "4",
+            "4",
+            "3",
+            "4"
+        ]
+    }
+]
+module.exports = friendList;
